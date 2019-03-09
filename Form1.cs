b89c91c06412629f3878e8cf7dd5479b5eb0aad3
Walkinged <43610419+Walkinged@users.noreply.github.com>
2019-03-09 18:54:24 +0000

    Add files via upload
    Email validity checker

---
+++ Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string stringemail = textBox1.Text;
+
+            int result = stringemail.IndexOf("@");
+
+            if (result == -1)
+            {
+                MessageBox.Show("Invalid Email Address!");
+            }
+
+            else
+            {
+                MessageBox.Show("Email address valid");
+                MessageBox.Show("@ found at position " + result.ToString());
+            }
+            
+            }
+    }
+}
